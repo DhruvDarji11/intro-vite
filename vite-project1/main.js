84b46eb0ba39cebf5d6b2e090e1586bd6f07b77d
Dhruv Darji <dhruvdarji1105@gmail.com>
2022-05-10 10:12:18 +0530

    vite

---
+++ vite-project1/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a> 
+  <a href="./HTML/Day7/Assignment/Resume.html" target="_blank">Resume </a>
+  <a href="./HTML/abc.html" target="_blank"> Htmlwork</a>
+  <a href="./HTML/HtmlNavigation.html" target="_blank"> Html and CSS Navigation</a>
+  <a href="./Bootstrap/bootstrap index.html" target="_blank"> Bootstrap</a>
+`
